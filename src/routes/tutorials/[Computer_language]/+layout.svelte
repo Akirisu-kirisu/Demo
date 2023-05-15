94c0a73deecb8571dd7e0ad0e9e29e63a6bffd56
akirisu-kirisu <regmangubat@outlook.com>
2023-05-16 06:43:33 +0800

    update

--- src/routes/tutorials/[Computer_language]/+layout.svelte
+++ src/routes/tutorials/[Computer_language]/+layout.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	import type { Config } from '@sveltejs/adapter-vercel';
+
+	export const config: Config = {
+		runtime: 'nodejs18.x'
+	};
 </script>
 
 <slot><!-- optional fallback --></slot>
