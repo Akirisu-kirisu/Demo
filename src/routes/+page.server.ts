51140e386b15aac85b99c9e19f6b9812107f7cc2
akirisu-kirisu <regmangubat@outlook.com>
2023-05-20 15:52:41 +0800

    GG

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,9 +2,9 @@ import { db } from '$lib/database/db';
 import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async () => {
 	return {
-		article: db.getJavaScriptContext(200),
+		article: db.getJavaScriptContext(100),
 		lazy: {
-			landing: db.getLandingPage(3000)
+			landing: db.getLandingPage(200)
 		}
 	};
 };
